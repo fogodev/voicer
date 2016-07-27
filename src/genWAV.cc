42b7e167f0cc4015a3e3247d18fa2089bd5afda0
Fernando Gouvea <fernandosrg1@gmail.com>
2016-07-27 16:54:50 -0300

    Changing genWav to recieve the sin and cos factors as parameters; Generating example files with genWav for testing purposes (128 samples each).

--- src/genWAV.cc
+++ src/genWAV.cc
@@ -8,14 +8,16 @@ void printArray(int size, double* array){
 	}
 }
 
-double f(double x){
-	return cos(3*x) + sin(10*x);
+double f(double x, int f_sin=10, int f_cos=3){
+	return cos(f_cos*x) + sin(f_sin*x);
 }
 
 int main (int argc, char** argv){
 	double* amps;
 	double sampleRate = 0.1;//in time units, should be named inverse of sampleRate ¯\_(ツ)_/¯
 	int size = 30;
+	int f_sin = 10; //default value, can be overwritten with an argument
+	int f_cos = 3;  //default value, ...
 
 	if(argc < 2){
 		printf("ERROR: No number of samples set\n");
@@ -25,10 +27,18 @@ int main (int argc, char** argv){
 		size = atoi(argv[1]);
 	}
 
+	if (argc > 2) {
+		f_sin = atoi(argv[2]);
+	}
+
+	if (argc > 3) {
+		f_cos = atoi(argv[3]);
+	}
+
 	amps = (double*) malloc(size * sizeof(double));
 
 	for(int i = 0; i < size; i++){
-		amps[i] = f(i*sampleRate);
+		amps[i] = f(i*sampleRate, f_sin, f_cos);
 	}
 
 	printf("%d\n", size);
